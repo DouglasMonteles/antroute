a002b473b687b8d74b1d64cc3bc94ea76bc3093d
DouglasMonteles <silvadouglas328@gmail.com>
2025-05-01 22:04:28 -0300

    :building_construction: configured server and agents modules
    Change-Id: I892c00fcf41caf11f903579bfac33b4cb03d745b

--- agents/src/main/java/br/com/doug/AgentApplication.java
+++ agents/src/main/java/br/com/doug/AgentApplication.java
@@ -1,13 +1,9 @@
 package br.com.doug;
 
-/**
- * Hello world!
- *
- */
-public class AgentApplication
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class AgentApplication {
+
+    public static void main(String[] args) {
+
     }
+
 }
