f5eeae420c399aa9d85570aa427956a9143d808f
DouglasMonteles <silvadouglas328@gmail.com>
2025-05-01 21:27:21 -0300

    :tada: init jade springboot project
    Change-Id: I1f7188dace049de11ba067d843107e9feada9264

---
+++ src/main/java/br/com/doug/antroute/AntrouteApplication.java
@@ -0,0 +1,13 @@
+package br.com.doug.antroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntrouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntrouteApplication.class, args);
+	}
+
+}
